5db7498e99b6a4006cc487f1ffcadef6f1b7f9d0
Surendra Pandey <sap172001@gmail.com>
2021-03-05 00:33:16 +0530

    Added the basic nomencleature

---
+++ School-management-system-flutter/lib/Screens/home.dart
@@ -0,0 +1,283 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:school_management/Screens/Exam/Exam_Rseult.dart';
+import 'package:school_management/Widgets/AppBar.dart';
+import 'package:school_management/Widgets/BouncingButton.dart';
+import 'package:school_management/Widgets/DashboardCards.dart';
+import 'package:school_management/Widgets/MainDrawer.dart';
+import 'package:school_management/Widgets/UserDetailCard.dart';
+
+import 'Attendance/Attendance.dart';
+import 'Leave_Apply/Leave_apply.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  Animation animation, delayedAnimation, muchDelayedAnimation, LeftCurve;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Firebase.initializeApp();
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    animationController =
+        AnimationController(duration: Duration(seconds: 3), vsync: this);
+    animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController, curve: Curves.fastOutSlowIn));
+
+    delayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn)));
+
+    muchDelayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.8, 1.0, curve: Curves.fastOutSlowIn)));
+
+    LeftCurve = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.5, 1.0, curve: Curves.easeInOut)));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+
+    animationController.forward();
+    return AnimatedBuilder(
+      animation: animationController,
+      builder: (BuildContext context, Widget child) {
+        final GlobalKey<ScaffoldState> _scaffoldKey =
+            new GlobalKey<ScaffoldState>();
+        return Scaffold(
+          key: _scaffoldKey,
+          drawer: Drawer(
+            elevation: 0,
+            child: MainDrawer(),
+          ),
+          appBar: CommonAppBar(
+            menuenabled: true,
+            notificationenabled: true,
+            ontap: () {
+              _scaffoldKey.currentState.openDrawer();
+            },
+            title: "Dashboard",
+          ),
+          body: ListView(
+            children: [
+
+              UserDetailCard(),
+              
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Transform(
+                transform: Matrix4.translationValues(
+                    muchDelayedAnimation.value * width, 0, 0),
+                child: DropdownSearch<String>(
+                  validator: (v) => v == null ? "Please Select" : null,
+                  hint: "Please Select",
+                  mode: Mode.MENU,
+                  showSelectedItem: true,
+                  
+                  items: [
+                    "Quarterly",
+                    "half yearly",
+                    "First Revision",
+                    'Second Revision',
+                    'Third Revision',
+                    'Annual Exam'
+                  ],
+                  showClearButton: false,
+                  onChanged: (value) {},
+                ),
+              ),
+              ),
+              
+              
+                      
+              // Padding(
+              //   padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+              //   child: Container(
+              //     alignment: Alignment(1.0, 0),
+              //     child: Padding(
+              //       padding: const EdgeInsets.only(top: 10.0, right: 20.0),
+              //       child: Row(
+              //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //         children: [
+              //           Transform(
+              //             transform: Matrix4.translationValues(
+              //                 muchDelayedAnimation.value * width, 0, 0),
+              //             child: Bouncing(
+              //               onPress: () {
+              //                 Navigator.push(
+              //                     context,
+              //                     MaterialPageRoute(
+              //                       builder: (BuildContext context) =>
+              //                           Attendance(),
+              //                     ));
+              //               },
+              //               child: DashboardCard(
+              //                 name: "Attendance",
+              //                 imgpath: "attendance.png",
+              //               ),
+              //             ),
+              //           ),
+              //           Transform(
+              //             transform: Matrix4.translationValues(
+              //                 delayedAnimation.value * width, 0, 0),
+              //             child: Bouncing(
+              //               onPress: () {},
+              //               child: DashboardCard(
+              //                 name: "Profile",
+              //                 imgpath: "profile.png",
+              //               ),
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+                child: Container(
+                  alignment: Alignment(1.0, 0),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10.0, right: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              muchDelayedAnimation.value * width, 0, 0),
+                          child: Bouncing(
+                            onPress: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        ExamResult(),
+                                  ));
+                            },
+                            child: DashboardCard(
+                              name: "Add Card",
+                              imgpath: "exam.png",
+                            ),
+                          ),
+                        ),
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              delayedAnimation.value * width, 0, 0),
+                          child: Bouncing(
+                            onPress: () {},
+                            child: DashboardCard(
+                              name: "Update Card",
+                              imgpath: "calendar.png",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+                child: Container(
+                  alignment: Alignment(1.0, 0),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10.0, right: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              muchDelayedAnimation.value * width, 0, 0),
+                          child: Bouncing(
+                            onPress: () {},
+                            child: DashboardCard(
+                              name: "All Cards",
+                              imgpath: "library.png",
+                            ),
+                          ),
+                        ),
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              delayedAnimation.value * width, 0, 0),
+                          child: Bouncing(
+                            onPress: () {},
+                            child: DashboardCard(
+                              name: "Delete Card",
+                              imgpath: "bus.png",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+                child: Container(
+                  alignment: Alignment(1.0, 0),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10.0, right: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              muchDelayedAnimation.value * width, 0, 0),
+                          child: Bouncing(
+                            onPress: () {},
+                            child: DashboardCard(
+                              name: "About Us",
+                              imgpath: "activity.png",
+                            ),
+                          ),
+                        ),
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              delayedAnimation.value * width, 0, 0),
+                          child: Bouncing(
+                            onPress: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        LeaveApply(),
+                                  ));
+                            },
+                            child: DashboardCard(
+                              name: "Share",
+                              imgpath: "leave_apply.png",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
